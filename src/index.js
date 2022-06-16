d5f83685ff5788d9ebbdc44ed76a1343333e7764
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-16 14:28:12 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleEvent(e.target.new - task - description.value);
+  });
+  let recentList = docuent.querySelector("#task");
+  let list = document.createElement("li");
+  let btn = document.createElement("button");
+  function handleEvent(work) {
+    list.textContent = `${work}`;
+    btn.textContent = "Done";
+    recentList.appendChild(list);
+    list.appendChild(btn);
+  }
 });
